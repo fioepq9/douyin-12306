5b4e2fb59c4c338a0ae8086294ce388742636852
codeReaper2001 <2210086948@qq.com>
2022-05-12 15:26:18 +0800

    添加注释

--- service/service.go
+++ service/service.go
@@ -11,12 +11,14 @@ func init() {
 	})
 }
 
+// InfoFlow 数据流接口，使用模板方法模式
 type InfoFlow interface {
 	checkParam() error
 	prepareInfo() error
 	packInfo() error
 }
 
+// FlowProcessor 流处理器，调用它的Do方法执行下面的校验参数、准备数据和封装结果的流程
 type FlowProcessor struct {
 	InfoFlow
 }
@@ -26,39 +28,20 @@ func (producer *FlowProcessor) SetInfoFlow(flow InfoFlow) {
 }
 
 func (producer FlowProcessor) Do() error {
+	// 校验参数
 	err := producer.checkParam()
 	if err != nil {
 		return err
 	}
+	// 准备数据
 	err = producer.prepareInfo()
 	if err != nil {
 		return err
 	}
+	// 封装结果
 	err = producer.packInfo()
 	if err != nil {
 		return err
 	}
 	return nil
 }
-
-//type InfoFlow interface {
-//	checkParam() error
-//	prepareInfo() error
-//	packInfo() error
-//}
-
-//func DoFlow(flow *InfoFlow) error {
-//	err := (*flow).checkParam()
-//	if err != nil {
-//		return err
-//	}
-//	err = (*flow).prepareInfo()
-//	if err != nil {
-//		return err
-//	}
-//	err = (*flow).packInfo()
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
