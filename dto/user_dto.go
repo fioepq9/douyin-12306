a666f9872aaaa4af220951b01e2a1c88d5ff7857
codeReaper2001 <2210086948@qq.com>
2022-05-14 23:39:16 +0800

    删除UserDTO

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -20,19 +20,3 @@ func (u *UserSimpleDTO) MarshalBinary() (data []byte, err error) {
 func (u *UserSimpleDTO) UnmarshalBinary(data []byte) error {
 	return json.Unmarshal(data, u)
 }
-
-type UserDTO struct {
-	Id            int64  `gorm:"column:id" json:"id,omitempty"`
-	Username      string `gorm:"column:username" json:"username,omitempty"`
-	Name          string `gorm:"column:name" json:"name,omitempty"`
-	FollowCount   int64  `gorm:"column:follow_count" json:"follow_count"`
-	FollowerCount int64  `gorm:"column:follower_count" json:"follower_count"`
-}
-
-func (u *UserDTO) MarshalBinary() (data []byte, err error) {
-	return json.Marshal(u)
-}
-
-func (u *UserDTO) UnmarshalBinary(data []byte) error {
-	return json.Unmarshal(data, u)
-}
